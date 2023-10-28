a8bf4db0e7c27ea0ba3e9cab67c22818893497c9
kulsoomrasheed <rasheedamaan111@gmail.com>
2023-10-28 16:41:56 +0530

    check score

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "MERN Stack Developer"
   ],
   description:
-    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results as a MERN (MongoDB, Express.js, React, Node.js) developer. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things in the world of web development.",
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/kulsoomrasheed",
   email:'rasheedamaan111@gmail.com',
   resume:
